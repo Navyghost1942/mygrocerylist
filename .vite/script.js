1cc7f373d62964736f69316764388d1c46db888c
NavyGhost1942 <riverawilliam256@gmail.com>
2023-09-03 20:16:23 -0500

    Fixed javascript

--- .vite/script.js
+++ .vite/script.js
@@ -1,42 +1,39 @@
-import {LocalDB} from 'https://cdn.skypack.dev/peadb';
-import shortid from 'https://cdn.skypack.dev/shortid';
-import canvasConfetti from 'https://cdn.skypack.dev/canvas-confetti';
+import { LocalDB } from 'https://cdn.skypack.dev/peadb'
+import shortid from 'https://cdn.skypack.dev/shortid'
+import confetti from 'https://cdn.skypack.dev/canvas-confetti'
 
-const db = new LocalDB('Grocery-list-db')
-
-const groceries = db.getAll || []
+const db = new LocalDB('grocery-list-db')
+const groceries = db.getAll() || []
 
 const groceryList = document.getElementById('groceryList')
 const newGroceryInput = document.getElementById('newGrocery')
 const addBtn = document.getElementById('addBtn')
 
 const createGroceryElement = grocery => {
-    const groceryElement = document.createElement('li')
-    groceryElement.innerText = grocery.value
-    groceryElement.classList.add('groceryItem')
-    groceryElement.addEventListener('click', () => {
-        groceryElement.remove()
-        db.delete(grocery.key)
-        canvasConfetti({particleCount: 300, spread: 1000, orgin: {y:1}})
-    })
-    return groceryElement
+  const groceryElement = document.createElement('li')
+  groceryElement.innerText = grocery.value
+  groceryElement.classList.add('groceryItem')
+  groceryElement.addEventListener('click', () => {
+    groceryElement.remove()
+    db.delete(grocery.key)
+    confetti({ particleCount: 300, spread: 1000, origin: { y: 1 } })
+  })
+  return groceryElement
 }
-
 
 const addGrocery = newGrocery => {
-    groceryList.appendChild(createGroceryElement)(newGrocery)
+  groceryList.appendChild(createGroceryElement(newGrocery))
 }
 
-
 addBtn.addEventListener('click', e => {
-    e.preventDefault()
-    const value = newGroceryInput.value
-    if(value){
+  e.preventDefault()
+  const value = newGroceryInput.value
+  if (value) {
     const key = shortid.generate()
-        addGrocery({key, value})
-        db.set(key,value)
-        newGroceryInput.value = null
-    }
+    addGrocery({ key, value })
+    db.set(key, value)
+    newGroceryInput.value = null
+  }
 })
 
 groceries.map(grocery => addGrocery(grocery))
